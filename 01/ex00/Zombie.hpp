d4cd9e204778d5b4321242b121138decbe972df8
Yuya Oda <yoda@c1r5s5.42tokyo.jp>
2024-04-25 20:57:47 +0900

    a

--- 01/ex00/Zombie.hpp
+++ 01/ex00/Zombie.hpp
@@ -6,7 +6,15 @@
 /*   By: yoda <yoda@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 17:30:44 by yoda              #+#    #+#             */
-/*   Updated: 2024/04/25 17:30:45 by yoda             ###   ########.fr       */
+/*   Updated: 2024/04/25 19:51:53 by yoda             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+
+class Zombie {
+	private:
+		std::string _name;
+	public:
+		std::string getName
+}
