6a1fa63a511d3d8d88f259c125eadbba9eb89e8e
oda251 <pgwyaueu@gmail.com>
2024-05-16 23:59:16 +0900

    a

--- 00/ex01/utils.hpp
+++ 00/ex01/utils.hpp
@@ -1,18 +1,7 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   utils.hpp                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yoda <yoda@student.42tokyo.jp>             +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/25 17:30:56 by yoda              #+#    #+#             */
-/*   Updated: 2024/05/16 18:58:54 by yoda             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef UTILS_HPP
 # define UTILS_HPP
 # include <iostream>
+# include <limits>
 
 bool		is_alnum(std::string str);
 bool		is_valid_phonenum_char(char c);
