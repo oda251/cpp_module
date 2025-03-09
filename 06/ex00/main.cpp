bc78a99857d0f8c4adca98f7f2b9add8f5e1db6f
Yuya Oda <yoda@c1r7s6.42tokyo.jp>
2025-03-10 05:25:19 +0900

    a

--- 06/ex00/main.cpp
+++ 06/ex00/main.cpp
@@ -1,26 +1,11 @@
 #include <iostream>
 #include "SchalarConverter.hpp"
 
-int main(void) {
-	std::cout << "--- 0 ---" << std::endl;
-	SchalarConverter::convert("0");
-	std::cout << "--- 42 ---" << std::endl;
-	SchalarConverter::convert("42");
-	std::cout << "--- -42 ---" << std::endl;
-	SchalarConverter::convert("-42");
-	std::cout << "--- 42.42f ---" << std::endl;
-	SchalarConverter::convert("42.42f");
-	std::cout << "--- 42.42 ---" << std::endl;
-	SchalarConverter::convert("42.42");
-	std::cout << "--- nan ---" << std::endl;
-	SchalarConverter::convert("nan");
-	std::cout << "--- -inf ---" << std::endl;
-	SchalarConverter::convert("-inf");
-	std::cout << "--- +inf ---" << std::endl;
-	SchalarConverter::convert("+inf");
-	std::cout << "--- -inff ---" << std::endl;
-	SchalarConverter::convert("-inff");
-	std::cout << "--- +inff ---" << std::endl;
-	SchalarConverter::convert("+inff");
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		std::cerr << "Usage: " << argv[0] << " <value>" << std::endl;
+		return 1;
+	}
+	SchalarConverter::convert(argv[1]);
 	return 0;
 }
